1195d73cc6993ce15e2bf9d19d4365361c30b279
Defani <adrien.defay@outlook.com>
2026-03-29 03:26:12 +0200

    Ajout: 	+ dessiner un cercle et un cerlce de couleur 	+ dessiner un disque et un disque de couleur

--- include/moteur/composant2d.h
+++ include/moteur/composant2d.h
@@ -5,6 +5,7 @@
 #include "GL/glut.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #include"config/config.h"
 
@@ -19,5 +20,13 @@ void draw_text_underlined(float x, float y, const char* text);
 void draw_text_color(float x, float y, const char* text, float r, float g, float b);
 
 void draw_text_underlined_color(float x, float y, const char* text,float r, float g, float b);
+
+void draw_circle(float cx, float cy, float r, int num_segments);
+
+void draw_disk(float cx, float cy, float r, int num_segments);
+
+void draw_colored_circle(float cx, float cy, float radius, int num_segments, float r_col, float g_col, float b_col);
+
+void draw_colored_disk(float cx, float cy, float radius, int num_segments, float r_col, float g_col, float b_col);
 
 #endif
