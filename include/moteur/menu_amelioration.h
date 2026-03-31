8c30bcaf71f7ac7649d56e5c5a1f349927add760
Gault115 <gaultierpeyrard2003@gmail.com>
2026-03-31 19:06:21 +0200

    Feat : ajout de l'affichage des compétence dans le menu pause

--- include/moteur/menu_amelioration.h
+++ include/moteur/menu_amelioration.h
@@ -4,6 +4,10 @@
 #include "moteur/composant2d.h"
 #include "moteur/affichage.h"
 
+extern char *description1;
+extern char *description2;
+extern char *description3;
+
 void aff_fen_amelioration();
 
 #endif /*_MENU_AMELIORATION_H_*/
--- src/moteur/menu_amelioration.c
+++ src/moteur/menu_amelioration.c
@@ -3,14 +3,28 @@
 
 #include "moteur/menu_amelioration.h"
 
+char *description1;
+char *description2;
+char *description3;
+
 void aff_fen_amelioration(){
     int ame_x = largeur_ecran/2-300;
     int ame_y = hauteur_ecran/2-250;
+
+    /*test*/
+    description1 = "+ 20% PV";
+    description2 = "+ 20% DEF";
+    description3 = "+ 20% ATQ";
+    /*fin test*/
+
     draw_rect(ame_x,ame_y,600,500,0,0,0, 0.7f);
     draw_text_underlined_color(ame_x + 187,ame_y + 30, "Choisissez une amélioration !",1,1,1);
     draw_rect(ame_x + 100,ame_y + 70,400,100,0,0,0, 1.0f);
     draw_rect(ame_x + 100,ame_y + 210,400,100,0,0,0, 1.0f);
     draw_rect(ame_x + 100,ame_y + 360,400,100,0,0,0, 1.0f);
+    draw_text_underlined_color(ame_x + 187,ame_y + 125, description1,1,0,0);
+    draw_text_underlined_color(ame_x + 187,ame_y + 265, description2,0,1,0);
+    draw_text_underlined_color(ame_x + 187,ame_y + 415, description3,0,0,1);
 }
 
 #endif /*_MENU_AMELIORATION_C_*/
